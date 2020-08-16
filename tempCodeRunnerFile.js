a6b4ecdf320e15e322e8a66990f4ffd5843dcbbd
Rudra <pollabrock@gmailcom>
2020-08-17 04:11:21 +0530

    .gitignore fix

--- tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-fs.readFile('./txt/outputfile.txt','utf-8', (err, data3) => {
-        //     fs.writeFile('./txt/nested.txt',`${data2}\n${data3}`, 'utf-8', err => {
-        //         console.log("nested file has been created!!!");
-        //     });
-        // });
